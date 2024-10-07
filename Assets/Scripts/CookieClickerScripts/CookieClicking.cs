f6c99c28931ebc1cccd859464c74d60929a09cb8
Arex2 <56590254+Arex2@users.noreply.github.com>
2024-10-07 18:06:08 +0200

    added cookie clicking canvas and script

---
+++ Assets/Scripts/CookieClickerScripts/CookieClicking.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieClicking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //on click
+    //+1 hp
+    //every second -1 hp
+
+    public void HPClick()
+    {
+        //add 1 hp to player hp
+    }
+}
