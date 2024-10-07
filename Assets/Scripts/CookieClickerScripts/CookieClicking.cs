d7168c1c8c27660b6c5d1cf167739006562b81d0
Arex2 <56590254+Arex2@users.noreply.github.com>
2024-10-07 18:44:57 +0200

    health and clicker pt1

--- Assets/Scripts/CookieClickerScripts/CookieClicking.cs
+++ Assets/Scripts/CookieClickerScripts/CookieClicking.cs
@@ -1,27 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class CookieClicking : MonoBehaviour
 {
+    PlayerHealth playerHealth;
+    GameObject panel;
+    GameObject button;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>();
+        panel = GameObject.Find("Panel");
+        button = GameObject.Find("Button");
+        HideCookieClicker();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            ShowCookieClicker();
+        }
+        if(Input.GetKeyUp(KeyCode.H))
+        {
+            HideCookieClicker();
+        }
     }
-
-    //on click
-    //+1 hp
-    //every second -1 hp
 
     public void HPClick()
     {
         //add 1 hp to player hp
+        playerHealth.AddHealth();
+    }
+
+    private void ShowCookieClicker()
+    {
+        //panel & button display
+        panel.SetActive(true);
+        button.SetActive(true);
+    }
+
+    private void HideCookieClicker()
+    {
+        panel.SetActive(false);
+        button.SetActive(false);
     }
 }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public bool loseHealth;
+    private int maxHealth = 10;
+    private int currentHealth = 10;
+    private float startTime = 1.5f; //time between 1hp draining
+    private float timer;
+
+    [SerializeField] private Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loseHealth = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (loseHealth)
+        {
+            //lose health
+            Timer();
+        }
+
+        if(currentHealth <= 0)
+        {
+            //player die
+        }
+    }
+
+
+    private void Timer()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0.0f)
+        {
+            MinusHealth();
+            timer = startTime;
+        }
+    }
+
+    public void AddHealth()
+    {
+        if (currentHealth >= maxHealth)
+            return;
+        currentHealth++;
+        UpdateHealthBar();
+    }
+
+    void MinusHealth()
+    {
+        currentHealth--;
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthSlider.value = currentHealth;
+
+        /*
+        if (currentHealth >= 2)
+        {
+            fillColor.color = Color.white;
+        }
+        else
+        {
+            fillColor.color = redHealth;
+        }
+        */
+    }
+}
