8eeee6d858ef6373c959ba11b9aa6a06c3e3ba5b
Liv Aronsson <liv.aronsson10@gmail.com>
2024-10-10 01:41:10 +0200

    lagt till sköld + script och en ny croissant sprite

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Shield : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 45f;
-    [SerializeField] private Transform rotateAround;
-    private bool autoRotate = true;
+    [SerializeField] private Transform player;
+    [SerializeField] private float activeTimer = 10f;
+    private float activeCountdownTimer;
+    public bool playerHasShield = false;
 
     private void Start()
     {
@@ -16,6 +18,25 @@ public class Shield : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        //this.transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        this.transform.RotateAround(player.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+
+        if (activeCountdownTimer > 0)
+        {
+            activeCountdownTimer -= Time.deltaTime;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            activeCountdownTimer = activeTimer;
+        }
     }
 }
